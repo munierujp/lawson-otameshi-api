f8861c763e0a2fd7a1310912accd762c16f90441
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-18 20:17:31 +0900

    Remove comments

--- src/createJSONResponse_.ts
+++ src/createJSONResponse_.ts
@@ -1,7 +1,5 @@
-// NOTE: Below function will be private in Google Apps Script.
 function createJSONResponse_ (obj: object): GoogleAppsScript.Content.TextOutput {
   return ContentService.createTextOutput(JSON.stringify(obj)).setMimeType(ContentService.MimeType.JSON)
 }
 
-// NOTE: Below statement will be removed by clasp.
 export default createJSONResponse_
