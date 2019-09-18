cc09ffcbff38dd2046f6cf512ae0836255aff12b
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-18 16:21:08 +0900

    Apply lint

--- src/doGet.ts
+++ src/doGet.ts
@@ -3,13 +3,15 @@ import isSameDay_ from './isSameDay_'
 import createJSONResponse_ from './createJSONResponse_'
 
 // NOTE: Below function will be public in Google Apps Script.
-function doGet (e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
-  const date = e.parameter.date // ISO 8601 (YYYY-MM-DD)
+function doGet (e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent): GoogleAppsScript.Content.TextOutput {
+  const params: any = e.parameter
+  const date: string = params.date // ISO 8601 (YYYY-MM-DD)
   let items = fetchItems_()
-  if (date) {
+  const isNotEmpty: (any) => boolean = value => typeof value !== 'undefined' && value !== ''
+  if (isNotEmpty(date)) {
     items = items.filter(item => isSameDay_(item.startDate, date))
   }
-  items = items.map(item => {
+  const resp = items.map(item => {
     return {
       title: item.title,
       image: item.image,
@@ -18,7 +20,7 @@ function doGet (e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
       detail: item.detail
     }
   })
-  return createJSONResponse_(items)
+  return createJSONResponse_(resp)
 }
 
 // NOTE: Below statement will be removed by clasp.
